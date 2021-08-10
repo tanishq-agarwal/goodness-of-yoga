62532f4ca23475caf4b3c24f0559ada4f431e162
tanishq-agarwal <agarwaltanishq35@gmail.com>
2021-08-10 17:51:16 +0530

    made website responsive free

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -3,50 +3,53 @@ import './Header.css';
 import { Portfolio } from "./Portfolio";
 import { Link } from "react-router-dom";
 
-export const Header = ({onBlur,onSubmit}) => {
+export const Header = ({ onBlur, onSubmit }) => {
 
     return (
-        <div className="header">
+        <div className="container-fluid header">
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="#">bre.ad/jane</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span class="dark-blue-text"><i
+                            class="fas fa-bars fa-1x"></i></span>
                     </button>
-                    <div className="d-flex mx-3">
-                        <ul className="navbar-nav d-lg-flex mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="#">Offerings</Link>
-                            </li>
-                            <li className="nav-item ">
-                                <Link className="nav-link" to="#" >
-                                    Other Links
-                                </Link>
-                            </li>
-                            <li className="nav-item ">
-                                <Link className="nav-link" to="#" >
-                                    Testimonials
-                                </Link>
-                            </li>
-                            <li className="nav-item ">
-                                <Link className="nav-link" to="#" >
-                                    Portfolio
-                                </Link>
-                            </li>
-                            <li className="nav-item ">
-                                <Link className="nav-link" to="#" >
-                                    Contact me
-                                </Link>
-                            </li>
-                        </ul>
-                        <button className="btn btn-outline-fffff" type="submit">Book Trial</button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+                        <div className="d-flex mx-3">
+                            <ul className="navbar-nav d-lg-flex mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="#">Offerings</Link>
+                                </li>
+                                <li className="nav-item ">
+                                    <Link className="nav-link" to="#" >
+                                        Other Links
+                                    </Link>
+                                </li>
+                                <li className="nav-item ">
+                                    <Link className="nav-link" to="#" >
+                                        Testimonials
+                                    </Link>
+                                </li>
+                                <li className="nav-item ">
+                                    <Link className="nav-link" to="#" >
+                                        Portfolio
+                                    </Link>
+                                </li>
+                                <li className="nav-item ">
+                                    <Link className="nav-link" to="#" >
+                                        Contact me
+                                    </Link>
+                                </li>
+                            </ul>
+                            <button className="btn btn-outline-fffff" type="submit">Book Trial</button>
+                        </div>
                     </div>
                 </div>
             </nav>
             <div className="text text-center">
                 Start your day with the goodess of Yoga
             </div>
-            <Portfolio onBlur={onBlur} onSubmit={onSubmit}/>
+            <Portfolio onBlur={onBlur} onSubmit={onSubmit} />
         </div>
     )
 }
--- src/MyComponents/Portfolio.js
+++ src/MyComponents/Portfolio.js
@@ -5,7 +5,7 @@ import { Offerings } from "./Offerings";
 import { Link } from "react-router-dom";
 
 console.log(pic);
-export const Portfolio = ({ onBlur, onSubmit }) => {
+export const Portfolio = ({onBlur,onSubmit}) => {
     let myStyle = {
         width: "150px",
         height: "150px",
@@ -27,46 +27,44 @@ export const Portfolio = ({ onBlur, onSubmit }) => {
                     <button type="button" className="btn btn-primary right">Book Trial</button>
                     <button className="btn btn-outline-primary right-bottom" type="button">Contact me</button>
                 </div>
-                <div className="content">
-                    <div className="text-left">
-                        <h2>Jane Doe</h2>
-                        <p>Hi, I am Jane Doe, your next door fitness instructor with just one goal.Make people fit and healthy.Yoga gurus from India introduced yoga to the West following the success of Vivekananda's adaptation of yoga without asanas in the late 19th and early 20th centuries
-                        </p>
+                <div className="text-left">
+                    <h2>Jane Doe</h2>
+                    <p>Hi, I am Jane Doe, your next door fitness instructor with just one goal.Make people fit and healthy.Yoga gurus from India introduced yoga to the West following the success of Vivekananda's adaptation of yoga without asanas in the late 19th and early 20th centuries
+                    </p>
+                </div>
+                <div className="icons">
+                    <Link to="#"><i class="fab fa-instagram"></i></Link>
+                    <Link to="#"><i class="fab fa-facebook-f"></i></Link>
+                    <Link to="#"><i class="fab fa-youtube"></i></Link>
+                    <Link to="#"><i class="fab fa-twitter"></i></Link>
+                    <Link to="#"><i class="fab fa-linkedin-in"></i></Link>
+                    <Link to="#"><i class="fab fa-spotify"></i></Link>
+                </div>
+                <hr className="line" />
+                <div className="residence">
+                    <i class="material-icons">&#xe55d;</i><h5>Lives in <span class="badge bg-light text-dark">New Delhi</span></h5>
+                </div>
+                <div className="language">
+                <i class="fa fa-language" aria-hidden="true"></i><h5>Speaks
+                        <span class="badge bg-light text-dark">English</span>
+                        <span class="badge bg-light text-dark">Hindi</span>
+                        <span class="badge bg-light text-dark">Punjabi</span>
+                    </h5>
+                </div>
+                <div className="row">
+                    <div className="d-grid justify-content-md-start video-text">Check out my video about 30 min yoga for beginners</div>
+                    <p class="text-sm-start">Contact me:</p>
+                    <div class="d-grid justify-content-md-end video"><div class="embed-responsive embed-responsive-21by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowFullScreen title="yoga-lecture"></iframe>
                     </div>
-                    <div className="icons">
-                        <Link to="#"><i class="fab fa-instagram"></i></Link>
-                        <Link to="#"><i class="fab fa-facebook-f"></i></Link>
-                        <Link to="#"><i class="fab fa-youtube"></i></Link>
-                        <Link to="#"><i class="fab fa-twitter"></i></Link>
-                        <Link to="#"><i class="fab fa-linkedin-in"></i></Link>
-                        <Link to="#"><i class="fab fa-spotify"></i></Link>
                     </div>
-                    <hr className="line" />
-                    <div className="residence">
-                        <i class="material-icons">&#xe55d;</i><h5>Lives in <span class="badge bg-light text-dark">New Delhi</span></h5>
-                    </div>
-                    <div className="language">
-                        <i class="fa fa-language" aria-hidden="true"></i><h5>Speaks
-                            <span class="badge bg-light text-dark">English</span>
-                            <span class="badge bg-light text-dark">Hindi</span>
-                            <span class="badge bg-light text-dark">Punjabi</span>
-                        </h5>
-                    </div>
-                    <div className="row">
-                        <div className="d-grid justify-content-md-start video-text">Check out my video about 30 min yoga for beginners</div>
-                        <p class="text-sm-start">Contact me:</p>
-                        <div class="d-grid justify-content-md-end video"><div class="embed-responsive embed-responsive-21by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="yoga-lecture" allowFullScreen></iframe>
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Subscribe to my profile and never miss another update from me.</label>
+                            <input type="email" class="form-control" id="email" placeholder="Enter your Email" aria-describedby="emailHelp" onBlur={()=> {onBlur()}} />
                         </div>
-                        </div>
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Subscribe to my profile and never miss another update from me.</label>
-                                <input type="email" class="form-control" id="email" placeholder="Enter your Email" aria-describedby="emailHelp" onBlur={() => { onBlur() }} />
-                            </div>
-                            <button type="submit" class="btn btn-primary" onClick={() => { onSubmit() }}>Subscribe</button>
-                        </form>
-                    </div>
+                        <button type="submit" class="btn btn-primary" onClick={()=> {onSubmit()}}>Subscribe</button>
+                    </form>
                 </div>
             </div>
             <Offerings />
